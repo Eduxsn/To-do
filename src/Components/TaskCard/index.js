b0ab3a1ae13e4dedfe2b21bdbfab0cea2399e03c
Eduardo <eduneves95@gmail.com>
2022-07-04 10:43:12 -0300

    Finished create and remove task functions

--- src/Components/TaskCard/index.js
+++ src/Components/TaskCard/index.js
@@ -3,28 +3,46 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export function TaskCard(props) {
     return (
-        <View style={styles.container}>
-            <View style={styles.checkName}>
-                <View style={styles.checkCard}></View>
-                <Text style={styles.itemName}>{props.taskName}</Text>
-            </View>
+        <View style={styles.shadow}>
+            <View style={styles.container}>
+                <View style={styles.checkName}>
+                    <View style={styles.checkCard}></View>
+                    <Text style={styles.itemName}>{props.taskName}</Text>
+                </View>
+                <View>
+                    <View style={styles.circle}></View>
+                </View>
 
-            
+                
+
+            </View>
 
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    shadow: {
+        shadowColor: '#0000001A',
+        shadowOffset: {width: 2, height: 4},
+        shadowOpacity: 0.6,
+        shadowRadius: 3,
+    },
     container: {
         backgroundColor: '#FFFFFF',
         borderRadius: 10,
         marginBottom: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between'
+        
     },
     checkName: {
         flexDirection: 'row',
         alignItems: 'center',
-        padding: 15
+        padding: 15,
+        flex: 'wrap',
+        maxWidth: '80%'
     },
     checkCard:{
         width: 24,
@@ -36,5 +54,13 @@ const styles = StyleSheet.create({
         fontSize: 14,
         paddingLeft: 15,
         // paddingVertical: 18
+    },
+    circle: {
+        height: 12,
+        width: 12,
+        borderColor: '#55BCF6',
+        borderWidth: 2,
+        marginRight: 15,
+        borderRadius: 3
     }
 })
--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,18 +1,54 @@
-import React from 'react';
-import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import React, { useState} from 'react';
+import { View, Text, StyleSheet, StatusBar, TouchableOpacity, TextInput, ScrollView, Keyboard, KeyboardAvoidingView, Platform } from 'react-native';
 import { TaskCard } from '../../Components/TaskCard/index';
 
 const statusBarHeight = StatusBar.currentHeight ? StatusBar.currentHeight + 22 : 94;
 
 
 export function Home() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Today`s tasks</Text>
-            <TaskCard taskName="Like"/>
-            <TaskCard taskName="Comprar pão"/>
-            <TaskCard taskName="Deploy app"/>
+    const [task, setTask] = useState('');
+    const [taskItems, setTaskItems] = useState([]);
 
+    const handleAddTask = () => {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task])
+        setTask(null);
+    }
+
+    const completeTask = (index) => {
+        let storedItens = [...taskItems];
+        storedItens.splice(index, 1);
+        setTaskItems(storedItens);
+    }
+
+    return (
+        
+        <View style={styles.container}>
+            <ScrollView showsVerticalScrollIndicator={false} style={styles.contentContainer}>
+                <Text style={styles.title}>Today`s tasks</Text>
+                {
+                    taskItems.map((item, index) => {
+                        return (
+                            <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                                <TaskCard taskName={item} />
+
+                            </TouchableOpacity>
+                        )
+                    })
+                }
+            </ScrollView>
+
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? 50 : 50}
+            >
+                <View style={styles.bottomArea}>
+                    <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
+
+                    <TouchableOpacity activeOpacity={.5} style={styles.button} onPress={() => handleAddTask()}>
+                        <Text style={styles.buttonText}>+</Text>
+                    </TouchableOpacity>
+                </View>
+            </KeyboardAvoidingView>
         </View>
     );
 }
@@ -29,5 +65,36 @@ const styles = StyleSheet.create({
         fontSize: 32,
         marginBottom: 30,
     },
-    
+    bottomArea: {
+        position: 'absolute',
+        bottom: 60,
+        width: '100%',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginLeft:20
+
+    },
+    input: {
+        backgroundColor: '#FFF',
+        width: 260,
+        paddingVertical: 15,
+        paddingHorizontal: 85,
+        borderRadius: 60
+    },
+    button: {
+        backgroundColor: '#FFF',
+        width: 60,
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 60,
+    },
+    buttonText: {
+        fontSize: 40,
+        color: '#C0C0C0'
+    },
+    contentContainer: {
+        marginBottom: 140,
+    }
 })
